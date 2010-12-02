82790d29dd761ef9dac06727b8650ed97b11c49b
Matt Mower <self@mattmower.com>
2010-12-02 21:36:23 +0000

    Moved the enabled test into the initialiser.

--- lib/errata.rb
+++ lib/errata.rb
@@ -1,12 +1,20 @@
+require 'errata/erratum'
 require 'errata/catcher'
-require 'errata/capture'
 require 'errata/logger'
 
 module Errata
   VERSION = "1.00"
   
+  def self.enabled?
+    true
+  end
+  
+  # def self.enabled?
+  #   !(Rails.env.development? || Rails.env.test?)
+  # end
+  
   def self.initialize
-    if defined?( ActionController::Base )
+    if enabled? && defined?( ActionController::Base )
       ActionController::Base.__send__( :include, Errata::Catcher )
     end
   end
