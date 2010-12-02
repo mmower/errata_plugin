397538ec7211eed358f9a54720af6241af3adb66
Matt Mower <self@mattmower.com>
2010-12-02 21:36:43 +0000

    Plugin writes an index file that the client can use to look up active servers.

--- lib/errata/logger.rb
+++ lib/errata/logger.rb
@@ -4,36 +4,54 @@ require 'json'
 module Errata
   
   class Logger
+    @@registered = false
     
-    def self.log_or_ignore( error, request )
-      # unless Rails.env.development? || Rails.env.test?
-        write( Capture.new( error, request ) )
-      # end
+    def self.errata_dir
+      dir = File.join( RAILS_ROOT, 'public', 'errata' )
+      FileUtils.mkdir_p( dir ) unless File.exists?( dir )
+      dir
     end
     
-    def self.write( capture )
-      errata_dir = File.join( RAILS_ROOT, 'public', 'errata' )
-      FileUtils.mkdir_p( errata_dir ) unless File.exists?( errata_dir )
-      
-      errata_file = File.join( errata_dir, "errata_#{capture.port}.json" )
-      
-      errata = if File.exists?( errata_file )
-        File.open( errata_file, "r" ) do |file|
-          json_source = file.read
-          if json_source.length > 0
-            JSON.parse( json_source )
+    def self.log_or_ignore( error, request )
+      write( Erratum.new( error, request ) )
+    end
+    
+    def self.read_json( file, default )
+      if File.exists?( file )
+        File.open( file, "r" ) do |file|
+          json = file.read
+          if json.length > 0
+            JSON.parse( json )
           else
-            []
+            default
           end
         end
       else
-        []
+        default
       end
-      
-      errata << capture
-      
-      File.open( errata_file, "w" ) do |file|
-        file.write( errata.to_json )
+    end
+    
+    def self.write_json( file, data )
+      File.open( file, "w" ) do |file|
+        file.write( data.to_json )
+      end
+    end
+    
+    def self.process_json( file, default, &blk )
+      write_json( file, yield( read_json( file, default ) ) )
+    end
+    
+    def self.register( capture )
+      process_json( File.join( errata_dir, "errata_idx.json" ), [] ) do |json|
+        json.push( capture.port ).uniq
+      end
+      @@registered = true
+    end
+    
+    def self.write( capture )
+      register( capture ) unless @@registered
+      process_json( File.join( errata_dir, "errata_#{capture.port}.json" ), [] ) do |json|
+        json.push( capture )
       end
     end
     
