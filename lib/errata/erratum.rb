dc6f48f51e721b37554356c1d3760d6abe3aa163
Matt Mower <self@mattmower.com>
2010-12-03 13:43:56 +0000

    Log the interesting bits of the request

--- lib/errata/erratum.rb
+++ lib/errata/erratum.rb
@@ -25,31 +25,38 @@ module Errata
         "QUERY_STRING"
       ]
     
-    attr_reader :port
     attr_reader :error
-    attr_reader :request_method
-    attr_reader :parameters
     attr_reader :time
-    attr_reader :env
     attr_reader :sha1
+    attr_reader :server_port
+    attr_reader :request
     
     def initialize( error, request )
-      @error = error
-      @time = Time.now
-      @port = Integer( request.env["SERVER_PORT"] )
-      @env = request.env
-      @request_method = request.request_method
-      @parameters = request.parameters
-      @sha1 = Digest::SHA1.hexdigest( "#{@port}-#{@time}-#{@error.message}-#{@parameters.inspect}")
+      @error       = error
+      @time        = Time.now
+      @server_port = Integer( request.env["SERVER_PORT"] || 0 )
+      @request     = request
+      @sha1        = Digest::SHA1.hexdigest( "#{@time}-#{@error.message}-#{@parameters.inspect}")
     end
     
     def to_json( *args )
       {
         'sha1' => sha1,
         'time' => time,
-        'port' => port,
-        'request_method' => request_method,
-        'parameters' => parameters,
+        'server_port' => port,
+        'request' => {
+          'remote_ip' => request.remote_ip,
+          'protocol' => request.protocol,
+          'host' => request.host,
+          'port' => request.port,
+          'domain' => request.domain,
+          'format' => request.format,
+          'method' => request.method,
+          'headers' => request.headers,
+          'url' => request.url,
+          'query_string' => request.query_string,
+        },
+        'parameters' => request.parameters,
         'error' => {
           'message' => error.message,
           'back_trace' => error.backtrace
